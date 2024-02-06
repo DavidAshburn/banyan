a565836641af60d1900a10fabf746c911c0f804c
DavidAshburn <oneinchwebbing@gmail.com>
2024-02-05 14:24:21 -1000

    working on config dependent create on sign-up

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,20 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :clients
-  has_one :profile
+  has_one :profile, dependent: :destroy
+  has_one :configuration, dependent: :destroy
   has_many :jobs
+
+  before_create :build_default_profile
+  before_create :build_default_configuration
+
+  private
+  def build_default_profile
+    profile = build_profile
+    true
+  end
+  def build_default_configuration
+    configuration = build_configuration
+    true
+  end
 end
