56b353fb0dd01bad0a2640ae8ae992303c697529
DavidAshburn <oneinchwebbing@gmail.com>
2024-02-07 20:17:19 -1000

    stubbed flatpickr into jobform

--- app/javascript/controllers/jobform_controller.js
+++ app/javascript/controllers/jobform_controller.js
@@ -1,6 +1,16 @@
 import { Controller } from '@hotwired/stimulus';
+import flatpickr from 'flatpickr';
 
 // Connects to data-controller="jobform"
 export default class extends Controller {
-  connect() {}
+  connect() {
+    const fpstart = flatpickr('#startdate', {
+      enableTime: true,
+      dateFormat: 'Y-m-d H:i',
+    }); //
+    const fpend = flatpickr('#enddate', {
+      enableTime: true,
+      dateFormat: 'Y-m-d H:i',
+    }); // flatpickr
+  }
 }
