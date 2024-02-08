edea1a11242f47a917c110fe888b53a061e8fdc4
DavidAshburn <oneinchwebbing@gmail.com>
2024-02-08 13:50:51 -1000

    profile progress;

--- app/javascript/components/Profile.jsx
+++ app/javascript/components/Profile.jsx
@@ -1,34 +1,64 @@
-import React, {useState, useEffect} from "react";
-import Windowpane from "./ui/Windowpane";
+import React, { useState, useEffect } from 'react';
+import Windowpane from './ui/Windowpane';
 
 export default function Profile() {
-    let [profile, setProfile] = useState({});
+  let [profile, setProfile] = useState({});
 
-    useEffect(() => {
-        fetch(`/data/profile`)
-          .then((response) => response.json())
-          .then((data) => {
-            setProfile(data);
-          });
-      }, []);
+  useEffect(() => {
+    fetch(`/data/profile`)
+      .then((response) => response.json())
+      .then((data) => {
+        setProfile(data);
+      });
+  }, []);
 
-    return(
-        <div className="grid lg:grid-cols-[1fr_6fr_1fr] min-h-screen bg-dark p-4 bg-light">
-            <div className="lg:col-start-2 min-h-[30vh]">
-                <Windowpane
-                title="User"
-                content={
-                    <div className="grid grid-rows-5">
-                        <p className="rounded border-b border-stone-50">Name: {profile.name}</p>
-                        <p className="rounded border-b border-stone-50">Species Saved: {profile.species}</p>
-                        <p className="rounded border-b border-stone-50">Vehicles: {profile.vehicles}</p>
-                        <p className="rounded border-b border-stone-50">Equipment: {profile.equipment}</p>
-                        <a href="/user/dashboard" className="darkbutton">Dashboard</a>
-                    </div>
-                }
-                light = 'o'
-                />
+  return (
+    <div className="grid lg:grid-cols-[1fr_6fr_1fr] min-h-screen bg-dark p-4 bg-light">
+      <div className="lg:col-start-2 min-h-[30vh]">
+        <div className="windowpanedark">
+          <p>User Profile</p>
+          <div>
+            <div className="grid grid-rows-5">
+              <p className="rounded border-b border-stone-50">
+                Name: {profile.name}
+              </p>
+              <div className="grid gap-2">
+                <p className="rounded border-b border-stone-50">
+                  Species Saved:
+                </p>
+                {profile.species ? (
+                  profile?.species.map((specie) => <p>{specie}</p>)
+                ) : (
+                  <div></div>
+                )}
+              </div>
+              <div className="grid gap-2">
+                <p className="rounded border-b border-stone-50">
+                  Vehicles:
+                </p>
+                {profile.vehicles ? (
+                  profile.vehicles.map((truck) => <p>{truck}</p>)
+                ) : (
+                  <div></div>
+                )}
+              </div>
+              <div className="grid gap-2">
+                <p className="rounded border-b border-stone-50">
+                  Equipment:
+                </p>
+                {profile.equipment ? (
+                  profile.equipment.map((item) => <p>{item}</p>)
+                ) : (
+                  <div></div>
+                )}
+              </div>
+              <a href="/user/dashboard" className="darkbutton">
+                Dashboard
+              </a>
             </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
