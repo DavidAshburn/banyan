ab44c0adce570474033abc46f82dd95bde5c12b4
DavidAshburn <oneinchwebbing@gmail.com>
2024-02-14 11:36:56 -1000

    dashboard maxZoom

--- app/javascript/controllers/dashjobsmap_controller.js
+++ app/javascript/controllers/dashjobsmap_controller.js
@@ -55,7 +55,7 @@ export default class extends Controller {
       bounds.extend(this.makeLL(item));
     }
     this.map.fitBounds(bounds, { padding: 100 });
-    this.map.setMaxZoom(18);
+    this.map.setMaxZoom(14);
     this.map.setMaxZoom(22);
   }
 
