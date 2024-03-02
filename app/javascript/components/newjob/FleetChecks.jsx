12f64ad5c312602b2d2e2d31090a4f7b09635fd2
DavidAshburn <oneinchwebbing@gmail.com>
2024-03-01 23:45:13 -1000

    bugfix in naming inside FleetChecks

--- app/javascript/components/newjob/FleetChecks.jsx
+++ app/javascript/components/newjob/FleetChecks.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 
 export default function FleetChecks({fleet, fleettype}) {
+    const lowertype = fleettype.toLowerCase(); 
+    let plural = fleettype;
+    if(fleettype == "Vehicle") plural = plural + 's';
 
     return(
         <div className="col-span-full">
-            <label htmlFor="vehicles">Vehicles</label>
-            <div className="grid grid-cols-2 gap-2 p-2" id="vehiclechecks">
+            <label>{plural}</label>
+            <div className="grid grid-cols-2 gap-2 p-2" id={`${lowertype}checks`}>
             {fleet.filter((item)=> item.fleettype === fleettype).map((fleetitem, i) => 
                 <div className="flex justify-between items-center text-end p-2 bg-accent text-light rounded" key={fleetitem.id}>
                 <input type="checkbox" value={fleetitem.name}/>
